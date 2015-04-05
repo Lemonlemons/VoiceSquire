48bfb9d79e600878a59836c7508ce42a19684eb8
unknown <moeandrew@hotmail.com>
2015-04-04 19:51:02 -0500

    seventh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => "young-citadel-2795.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://young-citadel-2795.herokuapp.com" }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => 'gmail.com',
+  :domain               => 'usesquire.com',
   :user_name            => 'moeandrew777@gmail.com',
   :password             => 'Megamoe7',
   :authentication       => 'plain',
