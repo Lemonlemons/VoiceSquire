5abf27af02539caa24c3a3f420fd7b503eca8fa4
unknown <moeandrew@hotmail.com>
2015-04-09 16:19:54 -0500

    more minor email changes

--- app/mailers/proposal_mailer.rb
+++ app/mailers/proposal_mailer.rb
@@ -1,15 +1,15 @@
 class ProposalMailer < ApplicationMailer
   def proof_email(quest)
     @quest = quest
-    mail(to: "moeandrew@hotmail.com", subject: 'Proof from Squire')
+    mail( to: "moeandrew@hotmail.com", subject: 'Proof from Squire')
   end
   def proposal_email(quest)
     @duke = Duke.where(id: quest.duke_id).first
     @quest = quest
-    mail(to: @duke.email, subject: 'Proposal from Squire')
+    mail( to: @duke.email, subject: 'Proposal from Squire')
   end
   def welcome_email(duke)
     @duke = duke
-    mail(to: @duke.email, subject: "Welcome to Squire")
+    mail( to: @duke.email, subject: "Welcome to Squire")
   end
 end
